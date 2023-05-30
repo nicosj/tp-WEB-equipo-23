a4566312c31e8e98129b88c9da9dcc658514af99
Erik Vilaplana <erik.vilaplana@alumnos.frgp.utn.edu.ar>
2023-05-30 01:34:17 -0300

    Agregado de Cards Listado con For Each

--- WebCarrito/Default.aspx.cs
+++ WebCarrito/Default.aspx.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using negocio;
+using Dominio;
+
 
 namespace WebCarrito
 {
     public partial class _Default : Page
     {
+
+        public List<Articulo> listaArticulos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            NegocioArticulo negocio = new NegocioArticulo();
+            listaArticulos= negocio.listarConSP();
         }
     }
 }
