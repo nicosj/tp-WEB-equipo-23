b62a5fc164e542f0fb4dabf86082cb72b4786338
Erik Vilaplana <erik.vilaplana@alumnos.frgp.utn.edu.ar>
2023-05-30 00:15:39 -0300

    Prueba 1

---
+++ WebCarrito/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebCarrito.Global" Language="C#" %>
--- negocio/NegocioArticulo.cs
+++ negocio/NegocioArticulo.cs
@@ -8,6 +8,46 @@ namespace negocio
 {
 	public class NegocioArticulo
 	{
+
+		public List<Articulo> listarConSP()
+		{
+            
+                List<Articulo> articulos = new List<Articulo>();
+                List<Marca> marcas = new List<Marca>();
+                List<Categoria> categorias = new List<Categoria>();
+
+                DB db = new DB();
+
+                try
+                {
+                    db.setearConsulta("SELECT Id, Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio FROM ARTICULOS");
+                    db.ejecutarLectura();
+
+                    while (db.Lector.Read())
+                    {
+                        Articulo aux = new Articulo();
+                        aux.Id = db.Lector.GetInt32(0);
+                        aux.Codigo = db.Lector.GetString(1);
+                        aux.Nombre = db.Lector.GetString(2);
+                        aux.Descripcion = db.Lector.GetString(3);
+                        aux.IdMarca = (int)db.Lector["IdMarca"];
+                        aux.IdCategoria = (int)db.Lector["IdCategoria"];
+                        aux.Precio = (decimal)db.Lector["Precio"];
+                        articulos.Add(aux);
+
+                    }
+                    db.cerrarConexion();
+                    return articulos;
+
+                }
+                catch (System.Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+
+
 		public List<Articulo> listar()
 		{
 			List<Articulo> articulos = new List<Articulo>();
