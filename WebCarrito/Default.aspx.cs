6c4c2093e48d1391dae331f264027c32acdf9fd3
Nicolas Rodriguez <nico.river09.9@gmail.com>
2023-05-30 00:59:51 -0300

    add:

--- WebCarrito/Default.aspx.cs
+++ WebCarrito/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using negocio;
 
 namespace WebCarrito
 {
@@ -11,7 +12,8 @@ namespace WebCarrito
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            NegocioMarca negocio = new NegocioMarca();
+            
         }
     }
 }
